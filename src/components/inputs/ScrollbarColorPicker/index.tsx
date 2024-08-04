7a77547eff51e96924e958c51c7c8062bb6fe12d
Eugene Domanovich <89357747+Dompower25@users.noreply.github.com>
2024-08-04 16:36:47 +0000

    ScrollbarColorPicker *in progres*

--- src/components/inputs/ScrollbarColorPicker/index.tsx
+++ src/components/inputs/ScrollbarColorPicker/index.tsx
@@ -1,14 +1,23 @@
-import React from "react"
+import React, { FC, useState } from "react"
 import style from './style.module.scss'
 
 //  при нажатии на colorPicker отображается окно с выбором цвета. Выбраный цвет отображается в формате HEX-кода
+interface IScrollbarColorPicker {
+  defaultColorInHexCode: string
+}
 
-const ScrollbarColorPicker = () => {
+const ScrollbarColorPicker: FC<IScrollbarColorPicker> = ({defaultColorInHexCode}) => {
+  const [color, setColor] = useState(defaultColorInHexCode)
+  
   return (
     <>
       <div className={style.wrapperColorPicker}>
-        <div className={style.colorPicker} />
-        #HEX-code
+        <div className={style.showColor} style={{
+          backgroundColor: `#${color}`
+        }} />
+        <input className={style.input} value={`#${color}`} onChange={(e) => {
+          setColor(e.target.value)
+        }}/>
       </div>
     </>
   )
