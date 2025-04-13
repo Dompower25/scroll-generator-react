d80c3c54dd776abc67aedec66d09e915ce8a673c
Eugene Domanovich <89357747+Dompower25@users.noreply.github.com>
2025-04-13 09:32:07 +0000

    *

--- src/components/inputs/ScrollbarColorPicker/index.tsx
+++ src/components/inputs/ScrollbarColorPicker/index.tsx
@@ -1,5 +1,6 @@
 import React, { FC, useCallback, useEffect, useState } from "react"
 import style from './style.module.scss'
+import {BlockPicker as ReactColorPicker} from 'react-color'
 
 //  при нажатии на colorPicker отображается окно с выбором цвета. Выбраный цвет отображается в формате HEX-кода
 //  по нажатию на div className={style.showColor} - появляется colorPicker в котором можно выбрать цвет и он добавится
@@ -16,12 +17,17 @@ const ScrollbarColorPicker: FC<IScrollbarColorPicker> = ({ defaultColorInHexCode
   const convertingToHex = (color: string): string => {
       return `#${color.length == 0 ? 'fff' : color.replace(/#/gi, '')}`
     }
+  
+  const openReactColorPicker = () => {
 
+    return <ReactColorPicker width="100px" />
+  }
 
   return (
     <>
       <div className={style.wrapperColorPicker}>
-        <div className={style.showColor} style={{
+        
+        <div className={style.showColor} onClick={openReactColorPicker} style={{
           backgroundColor: `${color}`
         }} />
         <input className={style.input} value={color} onChange={(e) => {
