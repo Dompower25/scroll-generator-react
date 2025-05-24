41caf8519c71912f9ef4da6ff8986f4b4d0a0a22
Eugene Domanovich <89357747+Dompower25@users.noreply.github.com>
2025-05-24 16:46:02 +0000

    created scrollbar component

---
+++ src/components/scrollbar/index.tsx
@@ -0,0 +1,18 @@
+import { FC } from "react";
+import style from './style.module.scss';
+
+interface IScrollbar {
+
+}
+
+const Scrollbar: FC<IScrollbar> = ({}) => {
+
+    return (
+        <>
+            
+        </>
+    )
+
+}
+
+export default Scrollbar;
